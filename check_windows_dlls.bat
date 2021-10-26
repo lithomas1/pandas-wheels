daa38bb960ec55ae2495ce6909b196af3a7b8a7e
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2021-10-26 08:22:42 -0700

    [skip ci] remove comments

--- check_windows_dlls.bat
+++ check_windows_dlls.bat
@@ -1,6 +1,4 @@
 python --version
-# Install dependencies first
 pip install pytz six numpy python-dateutil
-# Force install from our wheel
 pip install --find-links=pandas/pandas/dist --no-index pandas
 python -c "import pandas as pd; print(pd.__version__)"
